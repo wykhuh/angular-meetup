3b5e31dc07b516bbb28d887385e4eb7057b33fe4
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-03-16 00:23:07 -0700

    remove saved meetup

--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -9,6 +9,22 @@
   function HomeController($firebaseArray) {
     var ref = new Firebase("https://wyk-phil.firebaseio.com");
 
-    this.meetups = $firebaseArray(ref.child('meetups'));
+    var authData = ref.getAuth();
+
+    function createUsername(email) {
+      // Firebase doesn't allow some characters to be used as keys
+      return email.replace(/[\.#\$\[\]@]/g,'')
+    }
+
+    if(authData) {
+      this.remove = function(meetup) {
+        ref.child('userMeetup').child(username).child(meetup.$id).remove()
+      }
+      
+      var username = createUsername(authData.password.email);
+      this.meetups = $firebaseArray(ref.child('userMeetup').child(username));
+    } else {
+      this.meetups = [];
+    }
   }
 })();
