6bc700dfceee9e606306f7ccfcb8714fa0036a96
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-03-15 23:55:29 -0700

    save meeutp on all meetups

--- src/app/all_meetups/all_meetups.controller.js
+++ src/app/all_meetups/all_meetups.controller.js
@@ -9,6 +9,32 @@
   function AllMeetupsController($firebaseArray) {
     var ref = new Firebase("https://wyk-phil.firebaseio.com");
 
+
+    function createUserMeetup(username, meetup) {
+      ref.child('userMeetup').child(username).push({
+         title: meetup.title,
+         url: meetup.url,
+         id: meetup.$id
+      })
+    }
+
+    function createUsername(email) {
+      // Firebase doesn't allow some characters to be used as keys
+      return email.replace(/[\.#\$\[\]@]/g,'')
+    }
+
+    var authData = ref.getAuth();
+
     this.meetups = $firebaseArray(ref.child('meetups'));
+
+    // TODO: fix bug. save() sometimes saves one meetup twice
+    this.save = function save(meetup) {
+      if (authData) {
+        var email = authData.password.email;
+        var username = createUsername(email)
+        createUserMeetup(username, meetup);
+      }
+    }
+    
   }
 })();
