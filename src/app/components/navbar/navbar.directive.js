2926f6c58dc39c6e689612492884308729f52042
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-03-16 07:51:03 -0700

    add authData to nav

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -5,24 +5,25 @@
     .module('yoGulp')
     .directive('acmeNavbar', acmeNavbar);
 
-  /** @ngInject */
-  function acmeNavbar() {
-    var directive = {
-      restrict: 'E',
-      templateUrl: 'app/components/navbar/navbar.html',
-      scope: {
-          creationDate: '='
-      },
-      controller: NavbarController,
-      controllerAs: 'vm',
-      bindToController: true
-    };
-
-    return directive;
-
     /** @ngInject */
-    function NavbarController() {
-    }
-  }
+    function acmeNavbar() {
+      var directive = {
+        restrict: 'E',
+        templateUrl: 'app/components/navbar/navbar.html',
+        scope: {
+            authData: '='
+        },
+        controller: NavbarController,
+        controllerAs: 'vm',
+        bindToController: true
+      };
 
-})();
+      return directive;
+
+      /** @ngInject */
+      function NavbarController() {
+        this.authData = this.authData;
+      }
+    }
+
+  })();
